5defaeac3401e5d17c77541de1c72df7a9050813
M0stafa123 <jodat.ahmed135@gmail.com>
2024-02-16 13:22:33 +0200

    last in this branch

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -17,7 +17,7 @@ const About = () => {
         src="./145aeb42-a3b4-492a-bd0a-ea2b5bbf4312.webp"
         fetchpriority="high"
         alt="software"
-        className="software my-8  lg:max-w-[700px] mx-auto"
+        className="software my-8 hidden md:block lg:max-w-[600px] mx-auto"
       />
       <p className=" mb-10 capitalize text-4xl">
         build fast, responsive and dynamic websites
