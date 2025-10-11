1067df4c629a9d292f859e2db2db2760a115e5cf
M0stafa123 <jodat.ahmed135@gmail.com>
2025-10-11 19:52:00 +0300

    fix

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Card from "./Card";
+import Card from "./card";
 import allProjects from "../../projects.json";
 // import { Fade } from "react-awesome-reveal";
 import { Slide } from "react-awesome-reveal";
